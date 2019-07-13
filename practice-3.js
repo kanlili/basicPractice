0cc712424189d9ae94b4755c7c7f631cff2e4485
kanlili <51404596+kanlili@users.noreply.github.com>
2019-07-13 21:52:53 +0800

    upload section2-practice-3 test file

--- practice-3.js
+++ practice-3.js
@@ -1,15 +1,54 @@
 'use strict';
 
-module.exports = function collectSameElements(collectionA, objectB) {
-   var c=new Array();
-   var collectionB=objectB.value;
-   for(let i=0;i<collectionA.length;i++){
-     for (let j=0;j<collectionB.length;j++){
-      if (collectionA[i]==collectionB[j]){
-         c.push(collectionA[i]);
-         break;
-        }
-       }
-      }
-    return c;
-}
+module.exports = function countSameElements(collection) {
+            let c=[];
+            for(let item of collection){
+              if(item.length>1){
+               let {key,count}=split(item);
+               push(c,key,count);
+              }else{
+              c.push(item);
+              }
+            }
+           return count(c);
+         }
+         function split(ch){
+          if(ch.includes('-')){
+            let arr=ch.split('-');
+            return {key:array[0],count:parseInt(arr[1],10)};
+            }
+          if(ch.includes(":")){
+             let arr=ch.split(":");
+             return {key:array[0],count:parseInt(arr[1],10)};
+           }
+          if(ch.includes("[")){
+             let name = ch.charAt(0);
+             let summary = parseInt(ch.substr(2,ch.length-1));
+             return {name,summary};
+            }
+         }
+         function  push(c,key,count){
+           for(var i=0;i<count;i++){
+           c.push(key);
+           }
+         }
+         function count(collection){
+            let c=[];
+              for(let item of collection){
+                 let ele=find(c,item);
+                 if(ele){
+                   ele.count++;
+                 }else{
+                 c.push({key:item,count:1});
+                 }
+              }
+              return c;
+          }
+         function find(collection,ch){
+             for(let item of collection){
+                  if(item.key ==ch){
+                     return item;
+                  }
+             }
+              return null;
+         }
