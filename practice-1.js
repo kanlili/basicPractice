cafb63e6a60d718cda3b065dd7e75ae67b2961cc
kanlili <51404596+kanlili@users.noreply.github.com>
2019-07-13 21:52:18 +0800

    upload section2-practice-1 test file

--- practice-1.js
+++ practice-1.js
@@ -1,14 +1,22 @@
 'use strict';
 
-module.exports = function collectSameElements(collectionA, collectionB) {
-    var c=new Array();
-       for(var i=0;i<collectionA.length;i++){
-        for (var j=0;j<collectionB.length;j++){
-         if (collectionA[i]==collectionB[j]){
-          c.push(collectionA[i]);
-          break;
-         }
+module.exports = function countSameElements(collection) {
+  let c=[];
+     for(let item of collection){
+        let ele=find(c,item);
+        if(ele){
+          ele.count++;
+        }else{
+        c.push({key:item,count:1});
         }
-       }
-    return c;
+     }
+     return c;
+  }
+  function find(collection,ch){
+      for(let item of collection){
+           if(item.key ==ch){
+              return item;
+           }
+      }
+       return null;
 }
