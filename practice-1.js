5a617bbe8d6693eed3477fcb654ee202f7e0201e
kanlili <51404596+kanlili@users.noreply.github.com>
2019-07-13 21:51:33 +0800

    upload section1-practice-1 test file

---
+++ practice-1.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = function collectSameElements(collectionA, collectionB) {
+    var c=new Array();
+       for(var i=0;i<collectionA.length;i++){
+        for (var j=0;j<collectionB.length;j++){
+         if (collectionA[i]==collectionB[j]){
+          c.push(collectionA[i]);
+          break;
+         }
+        }
+       }
+    return c;
+}
